b471e2715e077060b69e152615c3f40d2adda190
Dimitri <deathmitri@gmail.com>
2017-09-25 23:18:08 +0700

    Finished parsing-data-2
    Release 0 and 1

--- parser.js
+++ parser.js
@@ -1,34 +1,109 @@
 "use strict"
 
+// require FileSystem
+let fs = require('fs');
+
+// require js-yaml
+let yaml = require('js-yaml');
+
 class Person {
-  // Look at the above CSV file
-  // What attributes should a Person object have?
+
+  constructor (id, firstName, lastName, email, phone, createdAt) {
+
+    this.id        = id;
+    this.firstName = firstName;
+    this.lastName  = lastName;
+    this.email     = email;
+    this.phone     = phone;
+    this.createdAt = createdAt;
+
+  }
+
 }
 
 class PersonParser {
 
   constructor(file) {
-    this.file = file
-    this.people = []
+    this._file = file;
+    this._people = [];
   }
 
   get people() {
-    // If we've already parsed the CSV file, don't parse it again.
-    if (this.people)
-      return this.people
 
-    // We've never called people before, now parse the CSV file
-    // and return an Array of Person objects here.  Save the
-    // Array in the this.people instance variable.
+    if (this._people.length)
+      return this._people
+
+    let readFromCSV = fs.readFileSync(this._file).toString().split('\n');
+
+    for (let i = 1; i < readFromCSV.length; i++) {
+
+      let record = readFromCSV[i].split(',');
+
+      this._people.push(new Person(
+
+        // id
+        parseInt(record[0]),
+        // firstName
+        record[1],
+        // lastName
+        record[2],
+        // email
+        record[3],
+        // phone
+        record[4],
+        // createdAt
+        record[5]
+
+      ));
+
+    }
+
+    return this._people;
+
   }
 
-  save() {}
+  save_as_yaml() {
 
-  save_as_yaml() {}
+    let yamlFormatting = {};
 
-  save_as_json() {}
+    let fetchWithoutId = function ({firstName, lastName, email, phone, createdAt}) {
+
+      return {firstName, lastName, email, phone, createdAt};
+
+    };
+
+    // Fetch objects and add it to yamlFormatting for dumping yaml
+    for (let i = 0; i < this._people.length; i++) {
+
+      yamlFormatting[this._people[i].id] = fetchWithoutId(this._people[i]);
+
+    }
+
+    // Generate new yaml file
+    fs.writeFileSync('people.yaml', yaml.dump(yamlFormatting));
+
+    console.log('Saved as yaml!');
+
+  }
+
+  save_as_json() {
+
+    fs.writeFileSync('people.json', JSON.stringify(this._people, null, '\t'));
+
+    console.log('Saved as JSON!');
+
+  }
 }
 
-var parser = new PersonParser('people.csv')
+var parser = new PersonParser('people.csv');
 
-console.log(`There are ${parser.people.size} people in the file '${parser.file}'.`)
+// console.log(`There are ${parser.people.size} people in the file '${parser.file}'.`)
+
+// Read from CSV
+parser.people;
+
+// Save as yaml
+parser.save_as_yaml();
+
+// Save as json
+parser.save_as_json();
