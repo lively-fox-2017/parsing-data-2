b9896ba750897027f86d8a0b19b403de37ea788c
Bambang Kurniawan <bembkurniawan@gmail.com>
2017-09-25 21:16:22 +0700

    parse to YAML and JSON--release 1

--- parser.js
+++ parser.js
@@ -1,34 +1,76 @@
 "use strict"
+const fs = require('fs');
+const yaml = require('js-yaml');
 
 class Person {
-  // Look at the above CSV file
-  // What attributes should a Person object have?
+    // Look at the above CSV file
+    // What attributes should a Person object have?
 }
 
 class PersonParser {
 
-  constructor(file) {
-    this.file = file
-    this.people = []
-  }
+    constructor(file) {
+        this._file = file;
+        this._people = [];
+    }
 
-  get people() {
-    // If we've already parsed the CSV file, don't parse it again.
-    if (this.people)
-      return this.people
+    readInput() {
+        const input = fs
+                        .readFileSync(this._file)
+                        .toString()
+                        .split('\n')
+                        .map(person => person.split(','));
 
-    // We've never called people before, now parse the CSV file
-    // and return an Array of Person objects here.  Save the
-    // Array in the this.people instance variable.
-  }
+        this._header = input.splice(0, 1)[0];
 
-  save() {}
+        input.forEach(person => {
+            let personObj = {};
+            for (let i = 0; i < this._header.length; i++) {
+                personObj[this._header[i]] = person[i];
+            }
+            this._people.push(personObj);
+        });
+    }
 
-  save_as_yaml() {}
+    get people() {
+        // If we've already parsed the CSV file, don't parse it again.
+        if (this._people) return this._people;
 
-  save_as_json() {}
+        // We've never called people before, now parse the CSV file
+        // and return an Array of Person objects here.  Save the
+        // Array in the this.people instance variable.
+    }
+
+    save() {
+        const formattedHeader = this._header.join(',') + '\n';
+        const formattedPeople = this._people
+                                            .map(person => person.join(','))
+                                            .join('\n');
+        
+        const formatted = formattedHeader + formattedPeople;
+        fs.writeFileSync(this._file, formatted);
+    }
+
+    save_as_yaml() {
+        let formatted = {};
+        this._people.forEach(person => {
+            formatted[person.first_name + ' ' + person.last_name] = person;
+        })
+        formatted = yaml.dump(formatted, {
+            indent: 4
+        });
+        fs.writeFileSync('people.yaml', formatted);
+    }
+
+    save_as_json() {
+        // 4 spaces rock!
+        const formatted = JSON.stringify(this._people, null, '    ');
+        fs.writeFileSync('people.json', formatted);
+    }
 }
 
 var parser = new PersonParser('people.csv')
-
-console.log(`There are ${parser.people.size} people in the file '${parser.file}'.`)
+parser.readInput();
+parser.save_as_json();
+parser.save_as_yaml();
+console.log(`There are ${parser.people.length} people in the file '${parser._file}'.`)
